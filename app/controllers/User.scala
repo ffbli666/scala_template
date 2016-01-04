c7c96c362dcf477dd7b55c32683b26539803e551
Zong-Ying Lyu <ffbli666@gmail.com>
2016-01-04 14:48:59 +0800

    added udpate, delete refactory

--- app/controllers/User.scala
+++ app/controllers/User.scala
@@ -1,72 +1,59 @@
+/*
+ref : http://mongodb.github.io/mongo-scala-driver/1.0/scaladoc/index.html#org.mongodb.scala.MongoCollection
+ */
 package controllers
 
-import models.User
+import models._
 import play.api._
 import play.api.mvc._
-import play.api.data.Form
-import play.api.data.Forms._
-import play.api.data.validation.Constraints._
 import play.api.libs.json._
 
-import org.mongodb.scala._
-import org.mongodb.scala.model.Aggregates._
-import org.mongodb.scala.model.Filters._
-import org.mongodb.scala.model.Projections._
-import org.mongodb.scala.model.Sorts._
-import org.mongodb.scala.model.Updates._
-import org.mongodb.scala.model._
-import org.bson.types.ObjectId
-
-import mongo.Helpers._
-
 class ApiUser extends Controller {
-    val mongoClient: MongoClient = MongoClient("mongodb://localhost")
-    val database: MongoDatabase = mongoClient.getDatabase("htc")
-    val collection: MongoCollection[Document] = database.getCollection("user")
-
-    val userForm: Form[User] = Form {
-        mapping(
-          "lastname"  -> nonEmptyText,
-          "firstname" -> nonEmptyText,
-          "email"     -> nonEmptyText,
-          "gender"    -> nonEmptyText,
-          "password"  -> nonEmptyText
-        )(User.apply)(User.unapply)
-    }
 
     def create = Action(parse.json) { implicit request =>
-        val user = userForm.bind(request.body).get
-        val doc: Document = Document(
-            "lastname"  -> user.lastname,
-            "firstname" -> user.firstname,
-            "email"     -> user.email,
-            "gender"    -> user.gender,
-            "password"  -> user.password
-        )
-        collection.insertOne(doc).results()
-        Ok("Got request [" + user + "]")
-    }
-
-    def search = Action {implicit request =>
-          Ok("Got request [" + request + "]")
-    }
-
-    def get(id: String) = Action {implicit request =>
-        val ret = collection.find(equal("_id", new ObjectId(id))).first().results()
+        UserModel.create(request.body);
         Ok(Json.parse("""
         {
           "status" : "ok",
-          "msg" : "success",
-          "result" : """ + ret.head.toJson() + """
+          "msg" : "success"
         }
         """))
     }
 
-    def update(id: String) = Action {implicit request =>
-          Ok("Got request [" + request.body + "]")
+    def search = Action { implicit request =>
+          Ok("Got request [" + request + "]")
     }
 
-    def delete(id: String) = Action {implicit request =>
-          Ok("Got request [" + request + "]")
+    def get(id: String) = Action { implicit request =>
+        val result = UserModel.get(id);
+        Ok(Json.parse("""
+        {
+          "status" : "ok",
+          "msg" : "success",
+          "result" : """ + result.toJson() + """
+        }
+        """))
+    }
+
+    def update(id: String) = Action(parse.json) { implicit request =>
+        var result = UserModel.update(id, request.body)
+        Ok(Json.parse("""
+        {
+          "status" : "ok",
+          "msg" : "success",
+          "result" : """ + result.toJson() + """
+        }
+        """))
+    }
+
+    def delete(id: String) = Action { implicit request =>
+        UserModel.delete(id);
+        Ok(Json.parse("""
+        {
+          "status" : "ok",
+          "msg" : "success",
+          "result" : """" + id + """"
+        }
+        """))
     }
 }
--- app/models/user.scala
+++ app/models/user.scala
@@ -1,15 +1,92 @@
 package models
 
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
 import play.api.libs.json._
 
-case class User(
+import org.mongodb.scala._
+import org.mongodb.scala.model.Aggregates._
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Projections._
+import org.mongodb.scala.model.Sorts._
+import org.mongodb.scala.model.Updates._
+import org.mongodb.scala.model._
+
+import org.bson.types.ObjectId
+
+import mongo.Helpers._
+
+case class User (
     firstname: String,
     lastname: String,
     email: String,
     gender: String,
-    password: String)
+    password: String
+)
 
-object User {
+case class UserUpdate (
+    firstname: String,
+    lastname: String,
+    gender: String,
+    password: String
+)
 
-  implicit val userFormat = Json.format[User]
+object UserModel {
+    val mongoClient: MongoClient = MongoClient("mongodb://localhost")
+    val database: MongoDatabase = mongoClient.getDatabase("htc")
+    val collection: MongoCollection[Document] = database.getCollection("user")
+
+    val userForm: Form[User] = Form {
+        mapping(
+          "lastname"  -> nonEmptyText,
+          "firstname" -> nonEmptyText,
+          "email"     -> email,
+          "gender"    -> nonEmptyText,
+          "password"  -> nonEmptyText
+        )(User.apply)(User.unapply)
+    }
+
+    def create (data: JsValue) {
+        val user = userForm.bind(data).get
+        val doc: Document = Document (
+            "lastname"  -> user.lastname,
+            "firstname" -> user.firstname,
+            "email"     -> user.email,
+            "gender"    -> user.gender,
+            "password"  -> user.password
+        )
+        collection.insertOne(doc).results()
+    }
+
+    def get(id: String) : Document = {
+        val result = collection.find(equal("_id", new ObjectId(id))).first().results()
+        return result.head
+    }
+
+    val userUpdateForm: Form[UserUpdate] = Form {
+        mapping(
+          "lastname"  -> nonEmptyText,
+          "firstname" -> nonEmptyText,
+          "gender"    -> nonEmptyText,
+          "password"  -> nonEmptyText
+        )(UserUpdate.apply)(UserUpdate.unapply)
+    }
+
+    def update(id: String, data: JsValue) : Document = {
+        val user = userUpdateForm.bind(data).get
+        collection.findOneAndUpdate(equal("_id", new ObjectId(id))
+                        , combine(
+                            Updates.set("lastname"   , user.lastname),
+                            Updates.set("firstname"  , user.firstname),
+                            Updates.set("gender"     , user.gender),
+                            Updates.set("password"   , user.password)
+                        )).results()
+        return this.get(id);
+    }
+
+    def delete(id: String) : Document = {
+        val result = collection.findOneAndDelete(equal("_id", new ObjectId(id))).results()
+        return result.head;
+    }
 }
